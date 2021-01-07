0a8e517cb4db8dcfb990a35e0ea7ff701d032213
snehaldwivedi <sdwivedi@msystechnologies.com>
2021-01-07 04:59:31 -0800

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,20 @@
+name              'mysql'
+maintainer        'Sous Chefs'
+maintainer_email  'help@sous-chefs.org'
+license           'Apache-2.0'
+description       'Provides mysql_service, mysql_config, and mysql_client resources'
+# source_url        'https://github.com/sous-chefs/mysql'
+# issues_url        'https://github.com/sous-chefs/mysql/issues'
+chef_version      '>= 12.7'
+version           '9.0.0'
+
+%w(redhat centos scientific oracle).each do |el|
+  supports el, '>= 7.0'
+end
+
+supports 'amazon'
+supports 'fedora'
+supports 'debian', '>= 9.0'
+supports 'ubuntu', '>= 16.04'
+supports 'opensuseleap'
+supports 'suse', '>= 12.0'
